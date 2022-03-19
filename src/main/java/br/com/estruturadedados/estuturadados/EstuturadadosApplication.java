9e4ba5448e5a13b5324cd639c57d24760401404e
juninhonori <norinrpo@gmail.com>
2022-03-19 12:28:30 -0300

    versao-3

--- src/main/java/br/com/estruturadedados/estuturadados/EstuturadadosApplication.java
+++ src/main/java/br/com/estruturadedados/estuturadados/EstuturadadosApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EstuturadadosApplication {
+	
 
 	public static void main(String[] args) {
 		Conjunto conjunto = new Conjunto();
